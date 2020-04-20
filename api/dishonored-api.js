fb32e949cdde851bd5080ab8f54f93b77fb9c8da
Pierre Vanhulst <pierre.vanhulst@icloud.com>
2020-04-20 18:12:24 +0200

    - Preview image & Sheet update - Details of the roll on mouseover for advanced - Moved roll button

--- api/dishonored-api.js
+++ api/dishonored-api.js
@@ -11,19 +11,22 @@ on('ready', () => {
             } catch (e) {
                 throw `Error: some arguments are missing or invalid.`;
             }
-
+            let results = [];
             let successes = 0;
             let passed = false;
             let momentum = 0;
             let dice = 0;
+            let crit = 0;
+            let threshold = 0;
             msg.inlinerolls.forEach(r => {
                 r.results.rolls.forEach(roll => {
                     dice = roll.dice;
-                    const crit = roll.mods.customCrit[0].point;
-                    const threshold = roll.mods.success.point;
-                    roll.results.forEach(die => {
-                        if (die.v <= crit) successes += 1;
-                        if (die.v <= threshold) successes += 1;
+                    crit = roll.mods.customCrit[0].point;
+                    threshold = roll.mods.success.point;
+                    results = [...roll.results.map(die => die.v)];
+                    results.forEach(die => {
+                        if (die <= crit) successes += 1;
+                        if (die <= threshold) successes += 1;
                     })
                 })
             })
@@ -34,7 +37,7 @@ on('ready', () => {
                 momentum = successes - difficulty;
             }
 
-            const command = `&{template:roll} {{skill=${skill}}} {{style=${style}}} {{focus=${focus}}} {{dice=${dice}}} {{difficulty=${difficulty}}} {{result=${successes}}} {{momentum=${momentum}}} {{class=${passed ? 'passed':'failed'}}} {{passed=${passed ? `passed, with ${momentum} Momentum generated` : 'failed'}}}`;
+            const command = `&{template:roll} {{skill=${skill}}} {{style=${style}}} {{focus=${focus}}} {{dice=${dice}}} {{difficulty=${difficulty}}} {{result=${successes}}} {{momentum=${momentum}}} {{class=${passed ? 'passed':'failed'}}} {{threshold=${threshold}}} {{crit=${crit}}} {{results=${results}}} {{passed=${passed ? `passed, with ${momentum} Momentum generated` : 'failed'}}}`;
 
             log('Debug');
             log(command);
